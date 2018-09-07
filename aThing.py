51285f2be17c63456368fe1b3607ac8579205b2a
Eric Wright <eric.wright@tooeleschools.org>
2018-09-07 09:36:12 -0600

    Added some comments

--- aThing.py
+++ aThing.py
@@ -9,6 +9,7 @@ print("my name is ", name, " and i like ", fact)
 message = "my name is " + name + " and I like " + fact
 print(message)
 
+# vars
 num = 8
 num2 = 14
 
@@ -17,6 +18,7 @@ print(num+num2)
 answer = num - num2
 print(answer)
 
+# vars
 word = "sweet"
 word1 = "cool"
 word2 = "mean"
@@ -26,12 +28,14 @@ word4 = "mate"
 print("I took a sip of " + word + " tea that was nice and " + word1 + ". With my " + word + " " + word1 + " tea I ate a " + word2 +
       " steak that was cooked to perfection. It started to get " + word3 + " so me and my " + word4 + " decided to call it a night.")
 
+# update vars to inputs
 word = input("Type an adjective ")
 word1 = input("Type a name ")
 word2 = input("Type another name ")
 word3 = input("Type a weapon ")
 word4 = input("Type another weapon ")
 
+# new madlib
 print("On a " + word + " night " + word1 + " was angerd at " + word2 + " because " + word2 + " wasn't being really nice. " + word1 + " decided to pickup a " + word3 +
       " and proceed to hit " + word2 + " with it. But " + word2 + " didn't like that so " + word2 + " decided to pick up a " + word4 + " and fight " + word1 + " with it. In an epic battle to the death " + word1 + " was victorious.")
 
